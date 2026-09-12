e349f72b6695a68d3a7646385d51012560b894be
Đoàn Minh Đại <daidoan1608@gmail.com>
2026-09-12 15:31:04 +0700

    fix(admin): support data and blob schemes in media url resolver

--- admin/src/utils/mediaUrl.js
+++ admin/src/utils/mediaUrl.js
@@ -2,7 +2,7 @@ import { ADMIN_API_ROOT } from "../config/env";
 
 export const getApiRoot = () => ADMIN_API_ROOT;
 
-export const isAbsoluteUrl = (url) => /^https?:\/\//i.test(url || "");
+export const isAbsoluteUrl = (url) => /^(?:https?:\/\/|data:|blob:)/i.test(url || "");
 
 export const resolveMediaUrl = (url, baseUrl = getApiRoot()) => {
   if (!url) return "";
